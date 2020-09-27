b976234aabee98a35900a2829c1b93dfb3466c49
jniconi 0e16e <jniconi@vlocity.com>
2020-09-27 21:54:14 +0800

    dashboard

---
+++ .history/dashboard/js/script_20200927111418.js
@@ -0,0 +1,386 @@
+var app = angular.module("quiz", ['chart.js', 'datatables']);
+
+
+app.controller("quizAdmin", function ($scope, $http) {
+
+/* 
+  $scope.groups =  [{
+    group_id:1,
+    group_name: 'Team A'
+  },
+  {
+    group_id:2,
+    group_name: 'Team B'
+  }
+]; */
+
+  $scope.getGroups = function () {
+    $http({
+      method: 'post',
+      url: 'processor/addremove.php',
+      data: {
+        request_type: 7
+      },
+    }).then(function successCallback(response) {
+      let groupsData = response.data;
+      console.log('groupsData', groupsData);
+     // $scope.allUsers = usersData;
+      $scope.groups = groupsData;
+      
+      
+    });
+  }
+  
+  $scope.getAcl = function () {
+    $http({
+      method: 'post',
+      url: 'processor/addremove.php',
+      data: {
+        request_type: 8
+      },
+    }).then(function successCallback(response) {
+      let aclData = response.data;
+      console.log('aclData', aclData);
+     // $scope.allUsers = usersData;
+      $scope.acl = aclData;
+      
+      
+    });
+  }
+  $scope.indiuserdata =[];
+  $scope.getIndividualUser = function (user_id) {
+   console.log('user_id', user_id);
+    $http({
+      method: 'post',
+      url: 'processor/addremove.php',
+      data: {
+        user_id: $scope.user_id,
+        request_type: 2
+      },
+    }).then(function successCallback(response) {
+      let usersData = response.data;
+      console.log('$scope.indiuserdata', usersData);
+     // $scope.allUsers = usersData;
+      $scope.indiuserdata = usersData;
+    })
+
+
+
+    $scope.labels = ["Earth", "Air", "Water", "Fire"];
+   /*  $scope.series = ['Susan', 'Joroni', 'Jane'];
+    $scope.data = [
+      [65, 15, 5, 15],
+      [10, 30, 41, 19],
+      [25, 20, 20, 35]
+    ]; */
+    
+    $scope.series = [];
+    for (var i = 0; i < $scope.indiuserdata.length; i++) {
+    
+      $scope.series.push(
+        $scope.indiuserdata[i].first_name,
+      );
+    }
+    console.log($scope.series );
+
+    $scope.data = [];
+    for (var i = 0; i < $scope.indiuserdata.length; i++) {
+    
+      $scope.data.push([
+        $scope.indiuserdata[i].user_earth,
+        $scope.indiuserdata[i].user_air,
+        $scope.indiuserdata[i].user_water,
+        $scope.indiuserdata[i].user_fire
+      ]);
+    }
+    console.log($scope.data );
+
+    $scope.onClick = function (points, evt) {
+      console.log(points, evt);
+    };
+    $scope.datasetOverride = [{
+      yAxisID: 'y-axis-1'
+    }, {
+      yAxisID: 'y-axis-2'
+    }];
+    $scope.options = {
+      scales: {
+        yAxes: [{
+            id: 'y-axis-1',
+            type: 'linear',
+            display: true,
+            position: 'left'
+          },
+          {
+            id: 'y-axis-2',
+            type: 'linear',
+            display: true,
+            position: 'right'
+          }
+        ]
+      }
+    };
+  }
+
+  
+
+  $scope.getUserScores = function () {
+    console.log('getUserScores');
+    $http({
+      method: 'post',
+      url: 'processor/addremove.php',
+      data: {
+        request_type: 1
+      },
+    }).then(function successCallback(response) {
+      let usersData = response.data;
+      console.log(usersData);
+     // $scope.allUsers = usersData;
+      $scope.userdata = usersData;
+      
+      $scope.labels = ["Earth", "Air", "Water", "Fire"];
+      //$scope.series = ['Susan', 'Joroni', 'Jane'];
+      $scope.series = [];
+      for (var i = 0; i < $scope.userdata.length; i++) {
+        $scope.series.push(
+          $scope.userdata[i].first_name,
+        );
+      }
+      console.log($scope.series );
+      $scope.data = [];
+      for (var i = 0; i < $scope.userdata.length; i++) {
+      
+        $scope.data.push([
+          $scope.userdata[i].user_earth,
+          $scope.userdata[i].user_air,
+          $scope.userdata[i].user_water,
+          $scope.userdata[i].user_fire
+        ]);
+      }
+      console.log($scope.data );
+      $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+      };
+      $scope.datasetOverride = [{
+        yAxisID: 'y-axis-1'
+      }, {
+        yAxisID: 'y-axis-2'
+      }];
+      $scope.options = {
+        scales: {
+          yAxes: [{
+              id: 'y-axis-1',
+              type: 'linear',
+              display: true,
+              position: 'left'
+            },
+            {
+              id: 'y-axis-2',
+              type: 'linear',
+              display: true,
+              position: 'right'
+            }
+          ]
+        }
+      };
+    });
+  }
+
+
+
+  $scope.series = [];
+//let user = [];
+$scope.onCheckboxChange = function(team, key) {
+ // console.log("Selected "+ team.user_id);
+  
+ /*  for (var i = 0; i < team.user_id.length; i++) {
+    console.log("Selected ", team.user_id);     
+    $scope.series.push([team.user_id]
+      
+    );
+  } 
+  console.log('series', $scope.series ); */
+
+  /* console.log(key +" changed to "+ team.key); */
+ // $scope.getAUserScore(user_id);
+};
+
+  $scope.getAUserScore = function (team) {
+ //   index = this.$index;
+   /*  self.select = function(index) {
+      $scope.series.push(index);
+  } */
+  /* $scope.series =[];
+  for (var i = 0; i < team.length; i++) {
+          
+    $scope.series.push(
+      team[i].user_id,
+    );
+  }
+  console.log('series', $scope.series );
+ */
+    
+    $http({
+      method: 'post',
+      url: 'processor/addremove.php',
+      
+      data: {
+        user_id:$scope.id,
+        request_type: 2
+      },
+    }).then(function successCallback(response) {
+      let usersData = response.data;
+      console.log(usersData);
+     // $scope.allUsers = usersData;
+      let auserdata = usersData;
+      
+      $scope.labels = ["Earth", "Air", "Water", "Fire"];
+      //$scope.series = ['Susan', 'Joroni', 'Jane'];
+
+/* 
+      for (var h = 0; h < team.length; h++) {
+      console.log(index.length);
+        
+ */
+          for (var i = 0; i < auserdata.length; i++) {
+          
+            $scope.series.push(
+              auserdata[i].user_id,
+            );
+          }
+          console.log('series', $scope.series );
+
+          $scope.data = [];
+          for (var i = 0; i < auserdata.length; i++) {
+          
+            $scope.data.push([
+              auserdata[i].user_earth,
+              auserdata[i].user_air,
+              auserdata[i].user_water,
+              auserdata[i].user_fire
+            ]);
+          }
+  //      }
+      console.log($scope.data );
+      $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+      };
+      $scope.datasetOverride = [{
+        yAxisID: 'y-axis-1'
+      }, {
+        yAxisID: 'y-axis-2'
+      }];
+      $scope.options = {
+        scales: {
+          yAxes: [{
+              id: 'y-axis-1',
+              type: 'linear',
+              display: true,
+              position: 'left'
+            },
+            {
+              id: 'y-axis-2',
+              type: 'linear',
+              display: true,
+              position: 'right'
+            }
+          ]
+        }
+      };
+    });
+  }
+
+
+
+
+  $scope.users=[];
+  $scope.addUserPop = false;
+  /* $scope.userid = $scope.fetchGetVariables().user_id;
+  $scope.groupid = $scope.fetchGetVariables().group_id; */
+  $scope.addUser = function () {
+   //let newuserdata = {};
+
+    /* 
+    console.log(angular.element('#MyAnswer')[0].value);
+    $scope.isanswer = angular.element('#MyAnswer')[0].value; */
+    /* len = $scope.answers.length; */
+    $scope.user_name=$scope.user_name;
+    $scope.email = $scope.email;
+    $scope.group_id = $scope.group_id;
+    /* for (let i = 0; i < userdata.length; i++) {
+      newuserdata.push({
+        'user_id': userdata[i].user_id,
+        'question_id':userdata[i].question_id,
+        'choice_id': $scope.isanswer,
+        'request_type': 2,
+        'len': len
+      });
+    } */
+    $http({
+      method: 'post',
+      url: 'processor/addremove.php',
+      //data: newuserdata
+      data: {
+        'user_name':  $scope.user_name,
+        'email':  $scope.email,
+        'first_name': $scope.first_name,
+        'last_name': $scope.last_name,
+        'group_id':$scope.group_id,
+        'acl':$scope.acl,
+        'request_type': 3
+      },
+    }).then(function successCallback(response) {
+      if (response.data.length > 0) {
+        $scope.users.push(response.data[0]);
+        alert('User added');
+      } else {
+        alert('User with this email already exist. ');
+      }
+    });
+  }
+
+
+
+
+  $scope.getUsers = function () {
+    $http({
+      method: 'post',
+      url: 'processor/addremove.php',
+      data: {
+        request_type: 4
+      },
+    }).then(function successCallback(response) {
+      let usersData = response.data;
+      console.log(usersData);
+     // $scope.allUsers = usersData;
+      $scope.allusers = usersData;
+      
+      
+    });
+  }
+
+
+  $scope.getUser = function (allusers) {
+    $scope.user_id = allusers.id;
+    $http({
+      method: 'post',
+      url: 'processor/addremove.php',
+      data: {
+        id: id,
+        request_type: 5
+      },
+    }).then(function successCallback(response) {
+      let userData = response.data;
+      console.log(userData);
+     // $scope.allUsers = usersData;
+      $scope.userowndata = userData;
+      
+      
+    });
+  }
+
+
+ /*  $http.get('fetch.php').success(function(data, status, headers, config){
+		$scope.customers = data;
+	}); */
+});
---
+++ .history/dashboard/processor/addremove_20200926165208.php
@@ -0,0 +1,328 @@
+<?php
+
+include '../../frontend/processor/config.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+$request_type = $data->request_type;
+
+// Get active question records
+if($request_type == 0){
+
+    $stmt = $con->prepare("SELECT * FROM questions WHERE is_active = 1");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = array();
+    if($result->num_rows > 0){
+       while($row = $result->fetch_assoc()) {
+            $data[] = array("question_id"=>$row['question_id'],"question"=>$row['question'],"type"=>$row['type'],"is_active"=>$row['is_active']); 
+        }
+    }
+    
+    $stmt->close();
+    echo json_encode($data);
+    exit;
+}
+
+
+if($request_type == 1){
+
+    $stmt = $con->prepare("SELECT * FROM `user_scores` INNER JOIN `users` WHERE users.id = user_scores.user_id");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = array();
+    if($result->num_rows > 0){
+       while($row = $result->fetch_assoc()) {
+            $data[] = array("user_id"=>$row['user_id'],"user_earth"=>$row['user_earth'],"user_air"=>$row['user_air'],"user_water"=>$row['user_water'], "user_fire"=>$row['user_fire'],"group_id"=>$row['group_id'],"first_name"=>$row['first_name'],
+            "last_name"=>$row['last_name'],"email"=>$row['email'],"created"=>$row['created']); 
+        }
+    }
+    
+    $stmt->close();
+    echo json_encode($data);
+    exit;
+}
+
+
+if($request_type == 2){
+    $user_id = $data->user_id; 
+    $stmt = $con->prepare("SELECT * FROM `user_scores` INNER JOIN `users` WHERE users.id = ? AND user_scores.user_id=?");
+    $stmt->bind_param('s',$user_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = array();
+
+
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()) {
+         $data[] = $row;
+         }
+     }
+
+  /*   if($result->num_rows > 0){
+       while($row = $result->fetch_assoc()) {
+            $data[] = array("user_id"=>$row['user_id'],"user_earth"=>$row['user_earth'],"user_air"=>$row['user_air'],"user_water"=>$row['user_water'], "user_fire"=>$row['user_fire'],"group_id"=>$row['group_id'],"first_name"=>$row['first_name'],
+            "last_name"=>$row['last_name'],"email"=>$row['email'],"created"=>$row['created']); 
+        }
+    }
+     */
+    $stmt->close();
+    echo json_encode($data);
+    exit;
+}
+
+// Insert record
+if($request_type == 3){
+    $user_name = $data->user_name; 
+    $email = $data->email; 
+    $first_name = $data->first_name;  
+    $last_name = $data->last_name;   
+    $group_id = $data->group_id;
+    $acl = $data->acl;
+    
+    
+    // Check answer to the question id already exists
+    $stmt = $con->prepare("SELECT * FROM users WHERE email=?");
+    $stmt->bind_param('s',$email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+    $return_arr = array();
+    if($result->num_rows == 0){
+       // INSERT INTO `users` (`id`, `user_name`, `user_password`, `email`, `first_name`, `last_name`, `group_id`, `date_joined`, `is_active`, `acl`) VALUES (NULL, 'jay', '', 'jay@mail.com', 'jay', 'yo', '2', '2020-09-25 15:07:19', '1', '2');
+        // Insert
+        //INSERT INTO `answers` (`id`, `user_id`, `question_id`, `choice_id`, `group_id`, `time_answered`) VALUES (NULL, '12', '16', '1234', '1', current_timestamp());
+        $insertSQL = "INSERT INTO users (user_name, email, first_name, last_name, group_id, acl) VALUES (?,?, ?, ?,?,?)";
+        $stmt = $con->prepare($insertSQL);
+        $stmt->bind_param("ssssss",$user_name,$email,$first_name,$last_name,$group_id,$acl);
+        $stmt->execute();
+
+        $lastinsert_id = $stmt->insert_id;
+        if($lastinsert_id > 0){
+            $return_arr[] = array("id"=>$lastinsert_id,"user_name"=>$user_name,"email"=>$email,"first_name"=>$first_name,"last_name"=>$last_name,"group_id"=>$group_id, "acl"=>$acl);
+        }
+        $stmt->close();
+        /*$insertSQL = "INSERT INTO users(fname,lname,username ) values(?,?,?)";
+        $stmt = $con->prepare($insertSQL);
+        $stmt->bind_param("sss",$fname,$lname,$username);
+        $stmt->execute();
+
+        $lastinsert_id = $stmt->insert_id;
+        if($lastinsert_id > 0){
+            $return_arr[] = array("id"=>$lastinsert_id,"fname"=>$fname,"lname"=>$lname,"username"=>$username);
+        }
+        $stmt->close();*/
+    }
+    
+    echo json_encode($return_arr);
+    exit;
+}
+
+// Get users' record
+if($request_type == 4){
+    $stmt = $con->prepare("SELECT * FROM users INNER JOIN groups WHERE users.group_id = groups.group_id ORDER BY users.id DESC");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = array();
+    if($result->num_rows > 0){
+       while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+        }
+    }
+    
+    $stmt->close();
+    echo json_encode($data);
+    exit;
+}
+
+
+
+// Get user by id
+if($request_type == 5){
+    $id = $data->id;
+    
+    // $email = 'jraymund.niconi@gmail.com';
+  
+     $stmt = $con->prepare("SELECT * FROM users WHERE id = ?");
+     $stmt->bind_param("s",$id);
+     $stmt->execute();
+     $result = $stmt->get_result();
+     $data = array();
+     if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()) {
+             $data[] = array("id"=>$row['id'],"first_name"=>$row['first_name'],"last_name"=>$row['last_name'],"email"=>$row['email'], "group_id"=>$row['group_id']); 
+         }
+     }
+     $stmt->close();
+     echo json_encode($data);
+     exit;
+ }
+/* 
+if ($btn_name == 'Update') {
+    $id    = $info->id;
+    $query = "UPDATE insert_emp_info SET name = '$name', email = '$email', age = '$age' WHERE id = '$id'";
+    if (mysqli_query($conn, $query)) {
+        echo 'Data Updated Successfully...';
+    } else {
+        echo 'Failed';
+    }
+} */
+
+
+
+
+// Get groupings
+if($request_type == 7){
+    $stmt = $con->prepare("SELECT * FROM groups");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = array();
+    if($result->num_rows > 0){
+       while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+        }
+    }
+    
+    $stmt->close();
+    echo json_encode($data);
+    exit;
+}
+
+
+// Get acls
+if($request_type == 8){
+    $stmt = $con->prepare("SELECT * FROM user_acl");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = array();
+    if($result->num_rows > 0){
+       while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+        }
+    }
+    
+    $stmt->close();
+    echo json_encode($data);
+    exit;
+}
+
+
+// Get user answer record
+if($request_type == 41){
+    $user_id = $data->user_id;
+    $stmt = $con->prepare("SELECT * FROM answers WHERE user_id = ? ORDER BY CAST(question_id AS unsigned)");
+    $stmt->bind_param('i',$user_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = array();
+    if($result->num_rows > 0){
+       while($row = $result->fetch_assoc()) {
+            $data[] = array("id"=>$row['id'],"user_id"=>$row['user_id'],"question_id"=>$row['question_id'],"choice_id"=>$row['choice_id']); 
+        }
+    }
+    
+    $stmt->close();
+    echo json_encode($data);
+    exit;
+}
+
+
+
+// Get user record
+if($request_type == 51){
+    $email = $data->email;
+    
+   // $email = 'jraymund.niconi@gmail.com';
+ 
+    $stmt = $con->prepare("SELECT * FROM users WHERE email = ?");
+    $stmt->bind_param("s",$email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = array();
+    if($result->num_rows > 0){
+       while($row = $result->fetch_assoc()) {
+            $data[] = array("id"=>$row['id'],"first_name"=>$row['first_name'],"email"=>$row['email'], "group_id"=>$row['group_id']); 
+        }
+    }
+    
+    $stmt->close();
+    echo json_encode($data);
+    exit;
+}
+
+if($request_type == 61){
+
+    $stmt = $con->prepare("SELECT * FROM users");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = array();
+    if($result->num_rows > 0){
+       while($row = $result->fetch_assoc()) {
+            $data[] = array("id"=>$row['id'],"first_name"=>$row['first_name'],"last_name"=>$row['last_name'],"user_name"=>$row['user_name'],"email"=>$row['email'],"group_id"=>$row['group_id']); 
+        }
+    }
+    
+    $stmt->close();
+    echo json_encode($data);
+    exit;
+}
+
+
+
+// Delete record
+if($request_type == 71){
+    $user_id = $data->user_id;
+
+    // Check userid exists
+    $stmt = $con->prepare("SELECT * FROM answers WHERE user_id=?");
+    $stmt->bind_param('i',$user_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+    
+    if($result->num_rows > 0){
+
+        // Delete
+        $deleteSQL = "DELETE FROM answers WHERE user_id=?";
+        $stmt = $con->prepare($deleteSQL);
+        $stmt->bind_param('i',$user_id);
+        $stmt->execute();
+        $stmt->close();
+
+        echo 1;
+        exit;
+    }else{
+        echo 0;
+    }
+
+    exit;
+}
+
+
+if($request_type == 9){
+    $stmt = $con->prepare("SELECT * FROM tbl_customer ORDER BY CustomerID DESC");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $data = array();
+    if($result->num_rows > 0){
+       while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+        }
+    }
+    
+    $stmt->close();
+    echo json_encode($data);
+    exit;
+/* $query = "SELECT * FROM tbl_customer ORDER BY CustomerID DESC";
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+while($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	$data[] = $row;
+}
+
+echo json_encode($data); */
+}
